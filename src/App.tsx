f2fc18d431afb094b1b26ef727a961652de30d63
ameliapham <ptt.huong.8895@gmail.com>
2025-05-27 23:23:23 +0200

    Setup tss react and update version of three and react three fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { tss } from "tss-react/mui";
 
 export function App() {
+
+    const { classes } = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
           <h1>Hello World</h1>
         </div>
       );
 }
+
+const useStyles = tss
+    .withName({ App })
+    .create(({ theme }) => ({
+        root: {
+            backgroundColor: "pink",
+            color: "black",
+            padding: theme.spacing(2),
+            textAlign: "center",
+        },
+    }));
