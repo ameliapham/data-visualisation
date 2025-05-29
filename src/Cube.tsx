923cbda67c9a8549e6388f5d84bfe3d34a85b616
ameliapham <ptt.huong.8895@gmail.com>
2025-05-29 17:01:43 +0200

    Update Cube with MeshTransmissionMaterial

--- src/Cube.tsx
+++ src/Cube.tsx
@@ -1,20 +1,41 @@
-type Props = {
-  position?: [number, number, number];
-  size?: [number, number, number];
-  color?: string;
-  children?: React.ReactNode;
-  rotation?: [number, number, number];
-  wireframe?: boolean;
-};
+import { useRef } from "react"
+import { Mesh } from "three"
+import { MeshTransmissionMaterial, RoundedBox } from "@react-three/drei"
 
-export function Cube(props: Props) {
-  const { children, color, position, size = [1, 1, 1], wireframe, ...rest } = props;
+interface CubeProps {
+  position?: [number, number, number]
+  size?: [number, number, number]
+}
+
+export function Cube({ 
+  position = [0, 0, 0], 
+  size = [1, 1, 1]
+}: CubeProps) {
+  const meshRef = useRef<Mesh>(null)
 
   return (
-    <mesh position={position} {...rest} >
-      <boxGeometry args={size}  />
-      <meshBasicMaterial color={color} wireframe={wireframe} />
-      {children}
-    </mesh>
-  );
+    <RoundedBox
+      ref={meshRef}
+      position={position}
+      args={size}
+      radius={0.1}
+      smoothness={4}
+    >
+      <MeshTransmissionMaterial
+        backside={true}
+        samples={4}
+        thickness={0.02}
+        transmission={0.95} 
+        roughness={0.05}
+        metalness={0}
+        clearcoat={0.1}
+        clearcoatRoughness={0.1}
+        ior={1.45} 
+        chromaticAberration={0.01}
+        distortion={0}
+        distortionScale={0}
+        temporalDistortion={0}
+      />
+    </RoundedBox>
+  )
 }
