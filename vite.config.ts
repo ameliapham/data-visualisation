60665cf57b065188c49575668d04d97d234e14b7
ameliapham <ptt.huong.8895@gmail.com>
2025-05-31 23:56:44 +0200

    delete vite config ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths' // Add this lign
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()], // Add tsconfigPaths() here too
-})
